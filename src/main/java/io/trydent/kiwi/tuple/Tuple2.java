5bf0d33d604f5d9975a62df97241949845337653
TryIO <io.trydent@gmail.com>
2016-09-02 16:38:03 +0200

    added travis configuration

--- src/main/java/io/trydent/kiwi/tuple/Tuple2.java
+++ src/main/java/io/trydent/kiwi/tuple/Tuple2.java
@@ -20,35 +20,43 @@ public final class Tuple2<T1, T2> implements Tuple {
 	private final T2 value2;
 
 	@SuppressWarnings("unchecked")
-	public static<T, R> Tuple2<T, R> empty() {
+	public static <T, R> Tuple2<T, R> empty() {
 		return (Tuple2<T, R>) EMPTY;
 	}
 
 	public Tuple2<T1, T2> filter(Predicate2<? super T1, ? super T2> filter) {
 		Objects.requireNonNull(filter);
-		return filter.satisfy(this.value1, this.value2)
-			? this
-			: empty();
+		if (filter.satisfy(this.value1, this.value2)) {
+			return this;
+		} else {
+			return empty();
+		}
 	}
 
 	public <U> Tuple1<U> map(Function2<? super T1, ? super T2, ? extends U> mapper) {
 		Objects.requireNonNull(mapper);
-		return Objects.isNull(this.value1) && Objects.isNull(this.value2)
-			? Tuple1.empty()
-			: new Tuple1<>(mapper.apply(this.value1, this.value2));
+		if (Objects.isNull(this.value1) && Objects.isNull(this.value2)) {
+			return Tuple1.empty();
+		} else {
+			return new Tuple1<>(mapper.apply(this.value1, this.value2));
+		}
 	}
 
 	public <U> Tuple1<U> flatMap1(Function2<? super T1, ? super T2, Tuple1<U>> mapper) {
 		Objects.requireNonNull(mapper);
-		return Objects.isNull(this.value1) && Objects.isNull(this.value2)
-			? Tuple1.empty()
-			: mapper.apply(this.value1, this.value2);
+		if (Objects.isNull(this.value1) && Objects.isNull(this.value2)) {
+			return Tuple1.empty();
+		} else {
+			return mapper.apply(this.value1, this.value2);
+		}
 	}
 
 	public <U1, U2> Tuple2<U1, U2> flatMap2(Function2<? super T1, ? super T2, Tuple2<U1, U2>> mapper) {
 		Objects.requireNonNull(mapper);
-		return Objects.isNull(this.value1) && Objects.isNull(this.value2)
-			? Tuple2.empty()
-			: mapper.apply(this.value1, this.value2);
+		if (Objects.isNull(this.value1) && Objects.isNull(this.value2)) {
+			return Tuple2.empty();
+		} else {
+			return mapper.apply(this.value1, this.value2);
+		}
 	}
 }
